1ee04ae98d461e784aa1c31ad889555aef4f3dbe
Valnati <workforthefuture@gmail.com>
2021-03-29 16:08:55 -0700

    navigation through app implemented; bottom nav bar implemented

--- app/src/main/java/com/androiddevs/runningappyt/ui/MainActivity.kt
+++ app/src/main/java/com/androiddevs/runningappyt/ui/MainActivity.kt
@@ -2,8 +2,13 @@ package com.androiddevs.runningappyt.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.androiddevs.runningappyt.R
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -21,5 +26,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //normally don't provide injection in the main activity - do it in the repository (MVVM)
+        setSupportActionBar(toolbar)
+
+        //assign navigation ability to bottomNaveBar
+        bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
+        //remove menu as needed for the different fragments
+        navHostFragment.findNavController()
+            .addOnDestinationChangedListener { _, destination, _ ->
+                when(destination.id) {
+                    R.id.settingsFragment, R.id.runFragment, R.id.statisticsFragment ->
+                        bottomNavigationView.visibility = View.VISIBLE
+                    else -> bottomNavigationView.visibility = View.INVISIBLE
+                }
+            }
     }
 }
