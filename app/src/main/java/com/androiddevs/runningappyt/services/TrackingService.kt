67465e1e509e19de4178629a3d53cca1c832623c
Valnati <workforthefuture@gmail.com>
2021-03-30 14:02:52 -0700

    created location pipeline, so that lat/long is calculated and saved every 5ish seconds

--- app/src/main/java/com/androiddevs/runningappyt/services/TrackingService.kt
+++ app/src/main/java/com/androiddevs/runningappyt/services/TrackingService.kt
@@ -1,5 +1,6 @@
 package com.androiddevs.runningappyt.services
 
+import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.NotificationManager.IMPORTANCE_LOW
@@ -7,24 +8,66 @@ import android.app.PendingIntent
 import android.app.PendingIntent.FLAG_UPDATE_CURRENT
 import android.content.Context
 import android.content.Intent
+import android.location.Location
 import android.os.Build
+import android.os.Looper
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import com.androiddevs.runningappyt.R
 import com.androiddevs.runningappyt.other.Constants.ACTION_PAUSE_SERVICE
 import com.androiddevs.runningappyt.other.Constants.ACTION_SHOW_TRACKING_FRAGMENT
 import com.androiddevs.runningappyt.other.Constants.ACTION_START_OR_RESUME_SERVICE
 import com.androiddevs.runningappyt.other.Constants.ACTION_STOP_SERVICE
+import com.androiddevs.runningappyt.other.Constants.FASTEST_LOCATION_INTERVAL
+import com.androiddevs.runningappyt.other.Constants.LOCATION_UPDATE_INTERVAL
 import com.androiddevs.runningappyt.other.Constants.NOTIFICATION_CHANNEL_ID
 import com.androiddevs.runningappyt.other.Constants.NOTIFICATION_CHANNEL_NAME
 import com.androiddevs.runningappyt.other.Constants.NOTIFICATION_ID
+import com.androiddevs.runningappyt.other.TrackingUtility
 import com.androiddevs.runningappyt.ui.MainActivity
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.maps.model.LatLng
 import timber.log.Timber
+
+typealias Polyline = MutableList<LatLng>
+typealias Polylines = MutableList<Polyline>
 
 class TrackingService : LifecycleService() {
 
     var isFirstRun = true
+
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    //similar to static objects
+    companion object {
+        val isTracking = MutableLiveData<Boolean>()
+        //livedata of list of list of coordinates to allow for start/stop, noncontiguous times
+        //the alias above makes this shorter to type
+        val pathPoints = MutableLiveData<Polylines>()
+    }
+
+    //populate coordinates at the starting line
+    private fun postInitialValues() {
+        isTracking.postValue(false)
+        pathPoints.postValue(mutableListOf())
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        postInitialValues()
+        fusedLocationProviderClient = FusedLocationProviderClient(this)
+        //class is lifecycle owner so context is this
+        isTracking.observe(this, Observer {
+            updateLocationTracking(it)
+        })
+    }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         //if intent is not null
@@ -51,8 +94,72 @@ class TrackingService : LifecycleService() {
         //need start, pause, stop actions
     }
 
+    //request location updates if given permissions to do so
+    @SuppressLint("MissingPermission")
+    private fun updateLocationTracking(isTracking: Boolean) {
+        if(isTracking) {
+            if(TrackingUtility.hasLocationPermissions(this)) {
+                val request = LocationRequest().apply {
+                    //theoretically every 5 seconds update, but this is not guaranteed
+                    //def every 2 seconds though, that is defined in constants
+                    interval = LOCATION_UPDATE_INTERVAL
+                    fastestInterval = FASTEST_LOCATION_INTERVAL
+                    priority = PRIORITY_HIGH_ACCURACY
+                }
+                fusedLocationProviderClient.requestLocationUpdates(
+                    request,
+                    locationCallback,
+                    Looper.getMainLooper()
+                )
+            }
+            //have stopped tracking so remove
+        } else {
+            fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+        }
+    }
+
+    //handles gathering location changes and adding to polylines
+    val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(result: LocationResult) {
+            super.onLocationResult(result)
+            //result should not be null, as it's being delivered to us
+            if(isTracking.value!!) {
+                //these could be though
+                result?.locations?.let { locations ->
+                    //if not add every location to current list
+                    for (location in locations) {
+                        addPathPoint(location)
+                        Timber.d("NEW LOCATION: ${location.latitude}, ${location.longitude}")
+                    }
+                }
+            }
+        }
+    }
+
+    //add location to last polyline of the current list of polylines
+    private fun addPathPoint(location: Location) {
+        //if it is not null
+        location?.let {
+            val pos = LatLng(location.latitude, location.longitude)
+            pathPoints.value?.apply {
+                last().add(pos)
+                pathPoints.postValue(this)
+            }
+        } //if null do nothing
+    }
+
+    //on a pause, add empty list to populate when next starting
+    private fun addEmptyPolyline() = pathPoints.value?.apply {
+        add(mutableListOf())
+        //broadcast new value of empty list
+        pathPoints.postValue(this)
+        //if list is null, add empty list and empty polyline to that list
+    } ?: pathPoints.postValue(mutableListOf(mutableListOf()))
+
     private fun startForegroundService() {
-        Timber.d("Beginning foreground service")
+        addEmptyPolyline()
+        //change mutable data with its own method
+        isTracking.postValue(true)
         //grab straight out of the framework and cast to a manager
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE)
             as NotificationManager
@@ -91,7 +198,6 @@ class TrackingService : LifecycleService() {
     //build channel with constants and start it in the manager
     @RequiresApi(Build.VERSION_CODES.O)
     private fun createNotificationChannel(notificationManager: NotificationManager) {
-        Timber.d("Android is O or above")
         val channel = NotificationChannel(
             NOTIFICATION_CHANNEL_ID,
             NOTIFICATION_CHANNEL_NAME,
